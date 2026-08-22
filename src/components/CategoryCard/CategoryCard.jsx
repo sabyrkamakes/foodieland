73d5c4f974261bc0ccc4fa1382ea69e938c5cb9a
Sabyrka <sabyrkamakes@gmail.com>
2026-08-22 22:58:52 +0500

    feat: add category card

---
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -0,0 +1,23 @@
+import './CategoryCard.scss'
+import { Image } from 'minista'
+
+export default (props) => {
+  const { title, imgSrc, color } = props
+
+  return (
+    <article className="category-card">
+      <div
+        className="category-card__body"
+        style={{ '--categoryCardColor': color }}
+      >
+        <div
+          className="category-card__image-wrapper"
+          style={{ '--categoryCardImage': `url('${imgSrc}')` }}
+        >
+          <Image className="category-card__image" src={imgSrc} />
+        </div>
+        <h3 className="category-card__title h6">{title}</h3>
+      </div>
+    </article>
+  )
+}
