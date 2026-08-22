c7b537e322d1a6a5530392564b931038962d292a
Sabyrka <sabyrkamakes@gmail.com>
2026-08-22 18:06:25 +0500

    feat: implement mobile menu module

---
+++ src/modules/MobileMenu.js
@@ -0,0 +1,33 @@
+class MobileMenu {
+  selectors = {
+    root: '[data-js-mobile-menu]',
+    overlay: '[data-js-mobile-menu-overlay]',
+    burgerButton: '[data-js-mobile-menu-burger-button]',
+  }
+
+  stateClasses = {
+    isActive: 'is-active',
+    isLock: 'is-lock',
+  }
+
+  onBurgerButtonClick = () => {
+    this.overlayElement.classList.toggle(this.stateClasses.isActive)
+    this.burgerButtonElement.classList.toggle(this.stateClasses.isActive)
+    document.documentElement.classList.toggle(this.stateClasses.isLock)
+  }
+
+  constructor() {
+    this.rootElement = document.querySelector(this.selectors.root)
+    this.overlayElement = this.rootElement.querySelector(this.selectors.overlay)
+    this.burgerButtonElement = this.rootElement.querySelector(
+      this.selectors.burgerButton
+    )
+    this.bindEvents()
+  }
+
+  bindEvents() {
+    this.burgerButtonElement.addEventListener('click', this.onBurgerButtonClick)
+  }
+}
+
+export default MobileMenu
