b3fded716a115c81ae7bf9eb6a1b58d30f8043ad
Sabyrka <sabyrkamakes@gmail.com>
2026-08-23 22:49:55 +0500

    feat(SubscribeBanner): add form and content structure

--- src/sections/SubscribeBanner/SubscribeBanner.jsx
+++ src/sections/SubscribeBanner/SubscribeBanner.jsx
@@ -1,5 +1,43 @@
 import './SubscribeBanner.scss'
+import Button from '@/components/Button'
 
 export default () => {
-  return <section className="subscribe-banner"></section>
+  return (
+    <section
+      className="subscribe-banner container"
+      aria-labelledby="subscribe-banner-title"
+    >
+      <div className="subscribe-banner__inner">
+        <div className="subscribe-banner__body">
+          <h2 className="subscribe-banner__title" id="subscribe-banner-title">
+            Deliciousness to your inbox
+          </h2>
+          <div className="subscribe-banner__description">
+            <p>
+              Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad
+              minim
+            </p>
+          </div>
+        </div>
+        <form className="subscribe-banner__form">
+          <div className="subscribe-banner__field">
+            <label className="visually-hidden" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="subscribe-banner__input"
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Your email address..."
+            />
+            <Button type="submit" mode="small">
+              Subscribe
+            </Button>
+          </div>
+        </form>
+      </div>
+    </section>
+  )
 }
