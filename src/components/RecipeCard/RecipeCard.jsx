d290cfeb529db1e14704df0db167e0f0626044b6
Sabyrka <sabyrkamakes@gmail.com>
2026-08-24 05:28:24 +0500

    refactor(RecipeCard): replace Image with lazy-loaded img element

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,6 +1,5 @@
 import './RecipeCard.scss'
 import clsx from 'clsx'
-import { Image } from 'minista'
 import Tags from '@/components/Tags'
 import heartFillIcon from '@/assets/icons/heart/heart-fill.svg'
 import heartOutlineIcon from '@/assets/icons/heart/heart-outline.svg'
@@ -11,19 +10,30 @@ export default (props) => {
   return (
     <div className={clsx('recipe-card', mode && `recipe-card--${mode}`)}>
       <div className="recipe-card__image-wrapper">
-        <Image className="recipe-card__image" src={imgSrc} />
+        <img
+          className="recipe-card__image"
+          src={imgSrc}
+          alt=""
+          width={mode === 'alt' ? 290 : 368}
+          height={mode === 'alt' ? 200 : 250}
+          loading="lazy"
+        />
         <button
           className="recipe-card__favorite-wrapper"
           type="button"
           aria-label={isFavorite ? 'Dislike' : 'Like'}
           title={isFavorite ? 'Dislike' : 'Like'}
         >
-          <Image
+          <img
             className={clsx(
               'recipe-card__favorite',
               isFavorite && 'is-favorite'
             )}
             src={isFavorite ? heartFillIcon : heartOutlineIcon}
+            alt=""
+            width={24}
+            height={24}
+            loading="lazy"
           />
         </button>
       </div>
