6d5923cca02422727140cf7f5f36e542f058b4a8
Sabyrka <sabyrkamakes@gmail.com>
2026-08-24 05:28:29 +0500

    refactor(PromoWidget): replace Image with lazy-loaded img elements

--- src/components/PromoWidget/PromoWidget.jsx
+++ src/components/PromoWidget/PromoWidget.jsx
@@ -1,13 +1,26 @@
 import './PromoWidget.scss'
-import { Image } from 'minista'
 import titleImage from '@/assets/images/promo-widget/title.svg'
 import dishImage from '@/assets/images/promo-widget/dish.png'
 
 export default () => {
   return (
     <div className="promo-widget">
-      <Image className="promo-widget__title" src={titleImage} />
-      <Image className="promo-widget__dish" src={dishImage} />
+      <img
+        className="promo-widget__title"
+        src={titleImage}
+        alt=""
+        width={166}
+        height={55}
+        loading="lazy"
+      />
+      <img
+        className="promo-widget__dish"
+        src={dishImage}
+        alt=""
+        width={198}
+        height={199}
+        loading="lazy"
+      />
       <a className="promo-widget__link" href="/">
         www.foodieland.com
       </a>
